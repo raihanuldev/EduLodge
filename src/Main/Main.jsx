cb8881ed4f30276b91ca9dace33030b5a7a88a42
Rihanul Islam <rihanthedev@gmail.com>
2023-07-22 02:10:19 +0600

    Footer

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../Components/Navbar/Navbar';
+import Footer from '../Components/Footer/Footer';
 
 const Main = () => {
     return (
         <div>
             <Navbar></Navbar>
            <Outlet></Outlet> 
+           <Footer></Footer>
         </div>
     );
 };
