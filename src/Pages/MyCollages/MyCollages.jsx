b1b67debed3b28b6649ba75139824b706e2f1014
Rihanul Islam <rihanthedev@gmail.com>
2023-07-24 02:19:22 +0600

    my-collage

--- src/Pages/MyCollages/MyCollages.jsx
+++ src/Pages/MyCollages/MyCollages.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import {AuthContex} from '../../Provider/AuthProvider';
 
 const MyCollages = () => {
+    const {user,loading} = useContext(AuthContex)
+    const [applications,setApplications] = useState(null)
+
+    useEffect(()=>{
+        fetch(`http://localhost:5000/applications?email=${user?.email}`)
+        .then(res=>res.json())
+        .then(data=>{
+            setApplications(data);
+        })
+    },[user])
+
+
     return (
         <div>
-            <h2>This is My Collages Page</h2>
+            <h2 className='text-3xl font-mono text-center my-3'>My Collages</h2>
+            <div>
+                {/* {
+                    applications.map((application,index))
+                } */}
+            </div>
         </div>
     );
 };
