0aecc71f0a150d8d92ba580f7bf057052bb00c9b
Rihanul Islam <rihanthedev@gmail.com>
2023-07-24 07:57:08 +0600

    my-collage

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -12,7 +12,7 @@ const firebaseConfig = {
   messagingSenderId: import.meta.VITE_messagingSenderId,
   appId: import.meta.env.VITE_appId
 };
-console.log(import.meta.VITE_apiKey);
+// console.log(import.meta.VITE_apiKey);
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app;
