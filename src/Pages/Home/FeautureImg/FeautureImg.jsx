5c16473156d14d4319515617a5360722b1ed429a
Rihanul Islam <rihanthedev@gmail.com>
2023-07-24 08:55:40 +0600

    image

--- src/Pages/Home/FeautureImg/FeautureImg.jsx
+++ src/Pages/Home/FeautureImg/FeautureImg.jsx
@@ -5,15 +5,15 @@ const FeautureImg = () => {
         <div className='px-5 rounded-lg'>
             <h2 className='text-4xl font-semibold text-center font-serif my-3'>Feature Image</h2>
             <div className='grid md:grid-cols-3 gap-1'>
-                <img className='h-72 rounded-lg' src="https://www.collegetransitions.com/wp-content/uploads/2014/05/shutterstock_789412672-1200x675.jpg" alt="" />
-                <img className='h-72 rounded-lg' src="https://media.istockphoto.com/id/1151567114/photo/a-young-female-graduate-against-the-background-of-university-graduates.jpg?s=612x612&w=0&k=20&c=zAFyR9UEbOWLOXqdNQSItVxVxOrjhOvwkfms-TyigA4=" alt="" />
-                <img className='h-72 rounded-lg' src="https://di-uploads-pod5.s3.amazonaws.com/beachautomotivegroup/uploads/2016/03/graduates.jpg" alt="" />
+                <img className='h-72 rounded-lg' src="https://images.pexels.com/photos/1205651/pexels-photo-1205651.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+                <img className='h-72 rounded-lg' src="https://images.pexels.com/photos/1139319/pexels-photo-1139319.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+                <img className='h-72 rounded-lg' src="https://images.pexels.com/photos/1184580/pexels-photo-1184580.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
                 <img className='h-72 rounded-lg' src="https://unity.edu/wp-content/uploads/2020/05/graduates-2015_0.jpg" alt="" />
-                <img  className='h-72 rounded-lg ' src="https://www.bankrate.com/2022/05/09185151/2022-college-graduation-statistics.jpg" alt="" />
-                <img  className='h-72 rounded-lg' src="https://www.vsu.edu/files/images/heroes/graduation_male_cap_gown_hero.jpg" />
-                <img className='h-72 rounded-lg' src="https://www.dominican.edu/sites/default/files/styles/width_1160/public/2020-02/graduation-registrar-hero.jpg?itok=l3Th9Xa8" />
-                <img className='h-72 rounded-lg' src="https://www.athens.edu/wp-content/uploads/2023/04/IMG_4251-scaled-e1681739733424.jpg" alt="" />
-                <img className='h-72 rounded-lg' src="https://www.pewresearch.org/wp-content/uploads/2022/04/FT_22.03.29_CollegeGradFacts__feature.jpg?w=640" alt="" />
+                <img  className='h-72 rounded-lg ' src="https://images.pexels.com/photos/7944238/pexels-photo-7944238.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+                <img  className='h-72 rounded-lg' src="https://images.pexels.com/photos/951289/pexels-photo-951289.jpeg?auto=compress&cs=tinysrgb&w=600" />
+                <img className='h-72 rounded-lg' src="https://images.pexels.com/photos/1699414/pexels-photo-1699414.jpeg?auto=compress&cs=tinysrgb&w=600" />
+                <img className='h-72 rounded-lg' src="https://images.pexels.com/photos/1628095/pexels-photo-1628095.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+                <img className='h-72 rounded-lg' src="https://images.pexels.com/photos/7713245/pexels-photo-7713245.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
             </div>
         </div>
     );
--- src/Pages/Home/TopCollage.jsx
+++ src/Pages/Home/TopCollage.jsx
@@ -3,30 +3,39 @@ import { FaSearch } from "react-icons/fa";
 import Collage from '../Collages/Collage';
 
 const TopCollage = () => {
-    const [defultCollage,setDefult] = useState([]);
-    const [searchFiled,setSearchFiled] = useState('');
+    const [defultCollage, setDefult] = useState([]);
+    const [searchFiled, setSearchFiled] = useState('');
+    const [searchData, setSearchData] = useState([])
 
-    const handleSearch =()=>{
-        console.log(searchFiled);
+    const handleSearch = () => {
+        // Fetch data based on the search field value
+        fetch(`http://localhost:5000/search-collages?query=${searchFiled}`)
+            .then(res => res.json())
+            .then(data => {
+                setSearchData(data);
+            });
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('http://localhost:5000/all-collages')
             .then(res => res.json())
             .then(data => {
                 setDefult(data)
             })
-    },[])
+    }, [])
     return (
         <div>
             <div className="flex w-2/3 mx-auto">
                 <input type="text" onChange={(e) => setSearchFiled(e.target.value)} className="input my-4 w-full bg-base-200" placeholder="Search Collage Name" />
-                <button onClick={handleSearch}  className="btn btn-outline ml-2 mt-4"> <FaSearch></FaSearch> </button>
+                <button onClick={handleSearch} className="btn btn-outline ml-2 mt-4"> <FaSearch></FaSearch> </button>
             </div>
             <h2 className='text-center text-3xl font-semibold'>Top Collage</h2>
             <div className='grid md:grid-cols-2'>
                 {
-                    defultCollage.map((collage,index)=> <Collage key={index} collage={collage}></Collage>).slice(0,3)
+                    // Display search results if available, otherwise display defaultCollage
+                    (searchData.length > 0 ? searchData : defultCollage)
+                        .map((collage, index) => <Collage key={index} collage={collage}></Collage>)
+                        .slice(0, 3)
                 }
             </div>
         </div>
