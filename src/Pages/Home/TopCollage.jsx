f382832a35a8f41a58c901e75018223c8ea83399
Rihanul Islam <rihanthedev@gmail.com>
2023-07-24 08:39:53 +0600

    SearchFiled Data

--- src/Pages/Home/TopCollage.jsx
+++ src/Pages/Home/TopCollage.jsx
@@ -4,6 +4,11 @@ import Collage from '../Collages/Collage';
 
 const TopCollage = () => {
     const [defultCollage,setDefult] = useState([]);
+    const [searchFiled,setSearchFiled] = useState('');
+
+    const handleSearch =()=>{
+        console.log(searchFiled);
+    }
 
     useEffect(()=>{
         fetch('http://localhost:5000/all-collages')
@@ -15,8 +20,8 @@ const TopCollage = () => {
     return (
         <div>
             <div className="flex w-2/3 mx-auto">
-                <input type="text" className="input my-4 w-full bg-base-200" placeholder="Search Collage Name" />
-                <button className="btn btn-outline ml-2 mt-4"> <FaSearch></FaSearch> </button>
+                <input type="text" onChange={(e) => setSearchFiled(e.target.value)} className="input my-4 w-full bg-base-200" placeholder="Search Collage Name" />
+                <button onClick={handleSearch}  className="btn btn-outline ml-2 mt-4"> <FaSearch></FaSearch> </button>
             </div>
             <h2 className='text-center text-3xl font-semibold'>Top Collage</h2>
             <div className='grid md:grid-cols-2'>
